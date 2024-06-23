a6b48a4a22f05a8140bdb2191baa76f6f78c6789
renato1958 <felisi.renato@gmail.com>
2024-06-23 22:24:15 +0200

    Set up vue-router

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+  </div>
+</template>
